542b4b0c6a057a95c5b328af000037539f0508b6
Michał Aleksandrowicz <michal.aleksandrowicz2609@gmail.com>
2021-10-24 12:53:13 +0200

    Base
    Adding the already done work like simple level, sprites, character animations, and player movement scripts.

---
+++ Wcale Nie Dead Cells/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum StateMachine
+    {
+        idle,
+        walk,
+        attack,
+        fall,
+        jump,
+        ledgeClimbing,
+        chainClimbing,
+        slide,
+        dead
+    }
+    [SerializeField]
+    internal PlayerMovement movement;
+    internal StateMachine currentState;
+    public float walkSpeed;
+    public float jumpForce;
+    public float slideForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentState = StateMachine.idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
